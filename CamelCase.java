5358563bbf7dd24554075345361735098db69beb
pbangandozou <119877277+pbangandozou@users.noreply.github.com>
2023-10-04 13:07:18 -0400

    Create CamelCase.java

---
+++ CamelCase.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'camelcase' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int camelcase(String s) {
+        int wordCount = 1; // First put word count to 1 
+
+    for (int i = 0; i < s.length(); i++) {
+        char currentChar = s.charAt(i);
+
+        // Check if the current character is an uppercase letter
+        if (Character.isUpperCase(currentChar)) {
+            wordCount++;
+        }
+    }
+
+    return wordCount;
+}
+
+}
+//CamelCase
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        int result = Result.camelcase(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
